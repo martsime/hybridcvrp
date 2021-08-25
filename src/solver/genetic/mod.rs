8966a21a29983ebc1f56149251b54fea149f02fe
Martin Simensen <martsime@users.noreply.github.com>
2021-08-25 11:14:14 +0200

    Remove project on gh-pages branch

--- src/solver/genetic/mod.rs
+++
@@ -1,8 +0,0 @@
-mod individual;
-pub use self::individual::*;
-mod genetic;
-pub use self::genetic::*;
-mod population;
-pub use self::population::*;
-mod split;
-pub use self::split::*;
--- src/utils/mod.rs
+++
@@ -1,8 +0,0 @@
-mod parser;
-pub use self::parser::*;
-
-mod random;
-pub use self::random::*;
-
-mod serializer;
-pub use self::serializer::*;
